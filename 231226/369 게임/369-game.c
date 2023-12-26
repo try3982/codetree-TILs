f7b123be4af535dd70a07ea94469b9bcce2db2f5
try3982 <78189431+try3982@users.noreply.github.com>
2023-12-26 16:20:30 +0900

    Title: 369 게임, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231226/369 게임/369-game.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n=0;
+    scanf("%d", &n);
+    for(int i=1; i <=n; i++) {
+        if(i %3 == 0) 
+            printf("0 ");
+        else if(i % 10 ==3 || i % 10 == 6|| i %3 == 9 )
+            printf("0 ");
+        else if(i / 10 ==3 || i / 10 == 6|| i /3 == 9 )
+            printf("0 ");
+        else
+            printf("%d ", i);
+
+    }
+    return 0;
+}
