b0b46b9edf3e42542a70e3228c1344bd94a1405f
try3982 <78189431+try3982@users.noreply.github.com>
2023-12-26 15:19:26 +0900

    Title: 문자열 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231226/문자열 출력하기/print-string.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n=0;
+    scanf("%d", &n);
+    for(int i=1; i <= n; i++) {
+        printf("LeebrosCode\n");
+    }
+    return 0;
+}
