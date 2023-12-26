9438cd5be0924d663208fe230701773baf2260f6
try3982 <78189431+try3982@users.noreply.github.com>
2023-12-26 16:25:34 +0900

    Title: 369 게임, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231226/369 게임/369-game.c
+++ 231226/369 게임/369-game.c
@@ -7,7 +7,7 @@ int main() {
     for(int i=1; i <=n; i++) {
         if(i %3 == 0) 
             printf("0 ");
-        else if(i % 10 ==3 || i % 10 == 6|| i %3 == 9 )
+        else if(i % 10 ==3 || i % 10 == 6|| i %10 == 9 )
             printf("0 ");
         else if(i / 10 ==3 || i / 10 == 6|| i /10 == 9 )
             printf("0 ");
