6ed4a16fb6b4de461487be215919734df538c025
try3982 <78189431+try3982@users.noreply.github.com>
2023-12-26 15:49:08 +0900

    Title: 2 또는 3의 배수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231226/2 또는 3의 배수/multiple-of-2-or-3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n =0;
+    scanf("%d", &n);
+    for(int i=1; i <=n; i++) {
+        if(i % 2 == 0 || i % 3 == 0) {
+            printf("1 ");
+        }else {
+            printf("0 ");
+        }
+    }
+    return 0;
+}
