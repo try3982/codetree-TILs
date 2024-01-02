ee70310c52a1dc62729d7563fc6ad1d3debe674b
try3982 <78189431+try3982@users.noreply.github.com>
2024-01-02 15:26:54 +0900

    Title: 배수의 개수 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240102/배수의 개수 2/number-of-multipliers-2.c
+++ 240102/배수의 개수 2/number-of-multipliers-2.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 
 int main() {
-    // 여기에 코드를 작성해주세요.
-    int cnt =0;
-    int n=0;
-    for(int i=1; i <=10; i++) {
-        scanf("%d", &n);
-        if(i % 2 != 0) {
-            cnt++;
-        }
-    }
-    printf("%d",cnt);
+    // 변수 선언
+    int a, cnt = 0;
+
+	// 입력
+	for(int i = 1; i <= 10; i++) {
+		scanf("%d", &a);
+		
+		if(a % 2 == 1)
+			cnt++;
+	}
+    
+    // 출력
+	printf("%d", cnt);
+
     return 0;
 }
