480c8b64f4108ba9aa783abd516e093a9e9ec8b3
try3982 <78189431+try3982@users.noreply.github.com>
2024-01-09 10:52:42 +0900

    Title: 별 찍는 것을 5번 반복하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240109/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.c
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+void Print10Stars() {
+    for(int i = 0; i < 10; i++)
+        cout << "*";
+    cout << endl;
+}
+
+int main() {
+
+    for(int i = 0; i < 5; i++)
+        Print10Stars(); 
+
+    return 0;
+}
