18f903d16af63cd7c908f295b0d2f38e635ca937
try3982 <78189431+try3982@users.noreply.github.com>
2024-01-22 15:36:31 +0900

    Title: 삼항연산자, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240122/삼항연산자/ternary-operator.c
+++ 240122/삼항연산자/ternary-operator.c
@@ -3,7 +3,7 @@
 int main() {
     // 여기에 코드를 작성해주세요.
     int score =0;
-    scanf("%d",&score)
-    rst = ((score == 100) ? "pass": "failure")
+    scanf("%d",&score);
+    rst = ((score == 100) ? "pass": "failure");
     return 0;
 }
