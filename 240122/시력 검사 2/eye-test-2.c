55a7f3344dab702b13d2bbb408ec642f6adec02d
try3982 <78189431+try3982@users.noreply.github.com>
2024-01-22 15:59:39 +0900

    Title: 시력 검사 2, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240122/시력 검사 2/eye-test-2.c
+++ 240122/시력 검사 2/eye-test-2.c
@@ -4,7 +4,7 @@ int main() {
     // 여기에 코드를 작성해주세요.
     float average = 0;
     scanf("%f",&average);
-    if(average >1.0) {
+    if(average >=1.0) {
         printf("High");
     }else if(average >0.5){
         printf("Middle");
