ce5d1925b3951925bc59ba2ae9028c71fd969e58
try3982 <78189431+try3982@users.noreply.github.com>
2024-01-22 16:28:07 +0900

    Title: 살 수 있는 물건 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240122/살 수 있는 물건 2/things-able-to-buy-2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n=0;
+    scanf("%d", &n);
+    if(n >=3000){
+        printf("book");
+    }else if(n >=1000) {
+        printf("mask");
+    }else if(n >= 500){
+        printf("pen");
+    }else {
+        printf("no");
+    }
+    return 0;
+}
