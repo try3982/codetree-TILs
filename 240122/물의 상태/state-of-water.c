2606b0cd9bffd1fba2b7f6c44b4fd627b3638019
try3982 <78189431+try3982@users.noreply.github.com>
2024-01-22 15:48:01 +0900

    Title: 물의 상태, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240122/물의 상태/state-of-water.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int ondo =0;
+    scnaf("%d", &ondo);
+    if(ondo < 0){
+        printf("ice");
+    }else if(ondo > 100) {
+        printf("vapor");
+    }else {
+        printf("water");
+    }
+    return 0;
+}
