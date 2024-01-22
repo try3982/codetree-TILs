20eaa28a8011bb711bb4ca72e41cb22c7a3295e7
try3982 <78189431+try3982@users.noreply.github.com>
2024-01-22 11:40:36 +0900

    Title: 시험 통과 여부 확인하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240122/시험 통과 여부 확인하기/verify-test-passed.c
+++ 240122/시험 통과 여부 확인하기/verify-test-passed.c
@@ -5,7 +5,7 @@ int main() {
     int score;
     scanf("%d", &score);
 
-    if(score >80) {
+    if(score >= 80) {
         printf("pass");
     }else {
         printf("%d more score", 80 - score);
