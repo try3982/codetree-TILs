6e5c0201198dadfce91d2c9003fa65989be3c781
try3982 <78189431+try3982@users.noreply.github.com>
2024-01-26 15:28:53 +0900

    Title: 원소 10개의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240126/원소 10개의 합/sum-of-10-elements.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n=0;
+    int sum=0;
+    int arr[10];
+
+    for(int i=0; i <10; i++) {
+        scanf("%d", &n);
+        sum += n;
+    }
+    printf("%d", sum);
+    return 0;
+}
