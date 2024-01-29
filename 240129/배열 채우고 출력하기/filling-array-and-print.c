4dfbbb7ac31710af5e1e3e012d11cc75824b953d
try3982 <78189431+try3982@users.noreply.github.com>
2024-01-29 13:44:05 +0900

    Title: 배열 채우고 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240129/배열 채우고 출력하기/filling-array-and-print.c
+++ 240129/배열 채우고 출력하기/filling-array-and-print.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 
 int main() {
-    // 여기에 코드를 작성해주세요.
+    // 크기가 10인 배열을 구현합니다.
     char arr[10];
-
-    for(int i=0; i <10; i++) {
-        scanf("%d",&arr[i]);
+	
+    // 0부터 9까지의 인덱스에 주어진 문자를 차례대로 입력받습니다.
+    for(int i = 0; i < 10; i++) {
+        scanf(" %c", &arr[i]);
     }
-    for(int i=10; i >10; i--) {
-        printf(arr[i]);
+	
+    // 9부터 0까지의 인덱스에 주어진 문자를 차례대로 출력합니다.
+    for(int i = 9; i >= 0; i--) {
+        printf("%c", arr[i]);
     }
+	
     return 0;
 }
