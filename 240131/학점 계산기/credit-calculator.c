c11e8dc63499f715e77cf2df01b9303a28ae2dd8
try3982 <78189431+try3982@users.noreply.github.com>
2024-01-31 11:09:08 +0900

    Title: 학점 계산기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240131/학점 계산기/credit-calculator.c
+++ 240131/학점 계산기/credit-calculator.c
@@ -1,21 +1,36 @@
 #include <stdio.h>
 
 int main() {
-    // 여기에 코드를 작성해주세요.
+    //과목의 개수 n을 입력받습니다.
     int n;
-    int avg;
-    scanf("%d",&n);
-    for(int i=0; i<n; i++) {
-        scanf("%f",&s);
-        if(s >=4.0) {
-            printf("Perfect");
-        }else if(s >=3.0) {
-            printf("Good");
-        }else{
-            printf("Poor");
-        }
-        
+    scanf("%d", &n);
+
+    // 변수 선언
+    double arr[5];
+    double sum = 0;
+
+	// 배열에 실수를 입력받아 저장합니다.
+    for(int i = 0; i < n; i++) {
+        scanf("%lf", &arr[i]);
     }
+	
+	// 배열에 있는 실수들의 합을 구합니다.
+	for(int i = 0; i < n; i++) {
+        sum += arr[i];
+    }
+
+    //평균 구하기
+    double avg = sum / n;
+
+    // 출력
+    printf("%.1lf\n", avg);
+
+    if(avg >= 4.0)
+        printf("Perfect");
+    else if(avg >= 3.0)
+        printf("Good");
+    else
+        printf("Poor");
 
     return 0;
 }
