97d8908bee44b049a42be3798bca1badb40b07d3
try3982 <78189431+try3982@users.noreply.github.com>
2024-01-31 11:08:21 +0900

    Title: 학점 계산기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240131/학점 계산기/credit-calculator.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    int avg;
+    scanf("%d",&n);
+    for(int i=0; i<n; i++) {
+        scanf("%f",&s);
+        if(s >=4.0) {
+            printf("Perfect");
+        }else if(s >=3.0) {
+            printf("Good");
+        }else{
+            printf("Poor");
+        }
+        
+    }
+
+    return 0;
+}
