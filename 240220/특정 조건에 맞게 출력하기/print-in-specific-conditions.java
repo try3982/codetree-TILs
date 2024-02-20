df4ffc490b9aeed5aa4f5709237b2bc026067500
try3982 <78189431+try3982@users.noreply.github.com>
2024-02-20 10:01:21 +0900

    Title: 특정 조건에 맞게 출력하기, Time: 144ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240220/특정 조건에 맞게 출력하기/print-in-specific-conditions.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        
+        int[] arr = new int[100];
+        int[] newArr = new int[100];
+        int zeroPoint = 0;
+
+         
+        for(int i = 0; i < 100; i++) {
+            arr[i] = sc.nextInt();
+            if(arr[i] == 0) {
+                zeroPoint = i;
+                break;
+            }
+            if(arr[i] % 2 == 0)
+                newArr[i] = arr[i] / 2;
+            else
+                newArr[i] = arr[i] + 3;
+        }
+
+         
+        for(int i = 0; i < zeroPoint; i++) {
+            System.out.print(newArr[i] + " ");
+        }
+    }
+}
