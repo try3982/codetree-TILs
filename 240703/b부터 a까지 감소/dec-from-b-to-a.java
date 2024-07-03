5a020c2e47b67887dbd70ee4e164e5e58c107512
try3982 <78189431+try3982@users.noreply.github.com>
2024-07-03 12:59:36 +0900

    Title: b부터 a까지 감소, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240703/b부터 a까지 감소/dec-from-b-to-a.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+    Scanner in= new Scanner(System.in);
+    int a = in.nextInt();
+    int b = in.nextInt();
+
+    for(int i=b; i>=a; i--) {
+        System.out.print(i + " ");
+    }
+
+
+    }
+}
