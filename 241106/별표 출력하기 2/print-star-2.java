226417afa56a4fb9318959e774193bd3bfa87898
try3982 <78189431+try3982@users.noreply.github.com>
2024-11-06 22:25:52 +0900

    Title: 별표 출력하기 2, Time: 140ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241106/별표 출력하기 2/print-star-2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        for(int i=1; i<=n; i++) {
+            for(int j=n; j >=i; j--) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
