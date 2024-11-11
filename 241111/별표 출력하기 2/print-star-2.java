b8078a902afe5844a8d8b9487c33bc563f11904a
try3982 <78189431+try3982@users.noreply.github.com>
2024-11-11 18:43:18 +0900

    Title: 별표 출력하기 2, Time: 123ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 241111/별표 출력하기 2/print-star-2.java
+++ 241111/별표 출력하기 2/print-star-2.java
@@ -6,10 +6,9 @@ public class Main {
         int n = in.nextInt();
 
         for(int i=0; i<n; i++) {
-            for(int j=0; j <n-i; j++) {
-                System.out.print("*");
+            for(int j=0; j<n-1; j++){
+                
             }
-            System.out.println("");
         }
     }
 }
